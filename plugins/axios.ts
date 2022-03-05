f59ec1a394481b453473b07d8bcb95d93595ec29
Frank Jogeleit <frank.jogeleit@web.de>
2022-03-05 19:59:48 +0100

    Use Hash Router for subpath support
    Signed-off-by: Frank Jogeleit <frank.jogeleit@web.de>

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -3,8 +3,12 @@ import qs from 'qs'
 
 const production = process.env.NODE_ENV === 'production'
 
+function trimSlashes (str: string) {
+  return str.split('/').filter(p => !!p).join('/')
+}
+
 const plugin: Plugin = ({ $axios }) => {
-  $axios.setBaseURL(production ? `//${window.location.host}` : `${process.env.NUXT_ENV_API_URL}`)
+  $axios.setBaseURL(production ? `//${window.location.host}/${trimSlashes(window.location.pathname)}` : `${process.env.NUXT_ENV_API_URL}`)
 
   $axios.onRequest((config) => {
     config.paramsSerializer = params => qs.stringify(params, { arrayFormat: 'repeat' })
