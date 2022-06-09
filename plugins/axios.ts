3c9c8158628050e68b1149246a2d03383396be65
Frank Jogeleit <frank.jogeleit@web.de>
2022-06-09 17:38:26 +0200

    Return 404 for non existing sub pages
    Signed-off-by: Frank Jogeleit <frank.jogeleit@web.de>

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,4 +1,5 @@
 import { Plugin } from '@nuxt/types'
+import { AxiosResponse } from 'axios'
 import qs from 'qs'
 
 const production = process.env.NODE_ENV === 'production'
@@ -9,6 +10,13 @@ function trimSlashes (str: string) {
 
 const plugin: Plugin = ({ $axios }) => {
   $axios.setBaseURL(production ? `//${window.location.host}/${trimSlashes(window.location.pathname)}` : `${process.env.NUXT_ENV_API_URL}`)
+  $axios.interceptors.response.use((response: AxiosResponse) => {
+    if (response.headers['content-type'] === 'text/html; charset=utf-8') {
+      return Promise.reject(new Error('unexpected content-type'))
+    }
+
+    return Promise.resolve(response)
+  })
 
   $axios.onRequest((config) => {
     config.paramsSerializer = params => qs.stringify(params, { arrayFormat: 'repeat' })
