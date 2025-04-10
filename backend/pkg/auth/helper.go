6a87780004c37bdbd7d5b7592ce3ba2699108a3e
Frank Jogeleit <frank.jogeleit@web.de>
2025-04-10 22:14:06 +0200

    add logging for group claim mapping (#509)
    Signed-off-by: Frank Jogeleit <frank.jogeleit@web.de>

--- backend/pkg/auth/helper.go
+++ backend/pkg/auth/helper.go
@@ -1,7 +1,10 @@
 package auth
 
 import (
+	"fmt"
+
 	"github.com/markbates/goth"
+	"go.uber.org/zap"
 )
 
 func mapGroups(user goth.User, claim string) []string {
@@ -13,11 +16,18 @@ func mapGroups(user goth.User, claim string) []string {
 
 	rawGroups, ok := user.RawData[claim]
 	if !ok {
+		keys := make([]string, 0, len(user.RawData))
+		for k := range user.RawData {
+			keys = append(keys, k)
+		}
+
+		zap.L().Warn("group claim not found", zap.Strings("available keys", keys))
 		return groups
 	}
 
 	mapped, ok := rawGroups.([]any)
 	if !ok {
+		zap.L().Error("unexpected group claim value, expected []any", zap.String("type", fmt.Sprintf("%T", mapped)))
 		return groups
 	}
 
@@ -26,6 +36,8 @@ func mapGroups(user goth.User, claim string) []string {
 			groups = append(groups, g)
 		}
 	}
+
+	zap.L().Debug("mapped user grous", zap.String("user", user.NickName), zap.String("email", user.Email), zap.Strings("groups", groups))
 
 	return groups
 }
