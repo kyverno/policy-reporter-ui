dc2aa43ba3bdc4cbae39b2ad4726e6e4b2d607ab
Frank Jogeleit <frank.jogeleit@web.de>
2025-04-28 15:06:15 +0200

    apply proxy configuration
    Signed-off-by: Frank Jogeleit <frank.jogeleit@web.de>

--- backend/pkg/api/http.go
+++ backend/pkg/api/http.go
@@ -10,6 +10,7 @@ import (
 func NewHTTPClient() *http.Client {
 	return &http.Client{
 		Transport: &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
 			DialContext: (&net.Dialer{
 				Timeout:   10 * time.Second,
 				KeepAlive: 60 * time.Second,
