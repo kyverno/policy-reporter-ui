6242f6a50ccee445569236cbb14354cc12f4234e
Frank Jogeleit <frank.jogeleit@web.de>
2025-04-28 14:32:12 +0200

    switch client constructor func
    Signed-off-by: Frank Jogeleit <frank.jogeleit@web.de>

--- backend/pkg/auth/factory.go
+++ backend/pkg/auth/factory.go
@@ -9,8 +9,6 @@ import (
 	"github.com/markbates/goth/providers/gitlab"
 	"github.com/markbates/goth/providers/google"
 	"github.com/markbates/goth/providers/yandex"
-
-	"github.com/kyverno/policy-reporter-ui/pkg/api"
 )
 
 type Options struct {
@@ -22,34 +20,34 @@ func NewProvider(provider string, clientKey string, secret string, callbackURL s
 	switch provider {
 	case "amazon":
 		p := amazon.New(clientKey, secret, callbackURL, scopes...)
-		p.HTTPClient = api.NewHTTPClient()
+		p.HTTPClient = NewHTTPClient()
 
 		return p
 	case "gitlab":
 		p := gitlab.New(clientKey, secret, callbackURL, scopes...)
-		p.HTTPClient = api.NewHTTPClient()
+		p.HTTPClient = NewHTTPClient()
 
 		return p
 	case "github":
 		p := github.New(clientKey, secret, callbackURL, scopes...)
-		p.HTTPClient = api.NewHTTPClient()
+		p.HTTPClient = NewHTTPClient()
 
 		return p
 	case "apple":
-		return apple.New(clientKey, secret, callbackURL, api.NewHTTPClient(), apple.ScopeName, apple.ScopeEmail)
+		return apple.New(clientKey, secret, callbackURL, NewHTTPClient(), apple.ScopeName, apple.ScopeEmail)
 	case "google":
 		p := google.New(clientKey, secret, callbackURL, scopes...)
-		p.HTTPClient = api.NewHTTPClient()
+		p.HTTPClient = NewHTTPClient()
 
 		return p
 	case "yandex":
 		p := yandex.New(clientKey, secret, callbackURL, scopes...)
-		p.HTTPClient = api.NewHTTPClient()
+		p.HTTPClient = NewHTTPClient()
 
 		return p
 	case "azuread":
 		p := azuread.New(clientKey, secret, callbackURL, nil, scopes...)
-		p.HTTPClient = api.NewHTTPClient()
+		p.HTTPClient = NewHTTPClient()
 
 		return p
 	}
